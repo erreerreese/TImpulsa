651718d7218fa83ee1079f6293ba3986588589ad
Ktl_XV <jorge@ktlxv.com>
2014-11-06 15:20:03 -0600

    Materiales styles, iteration 1

--- js/controllers/presentation-thumbnails.js
+++ js/controllers/presentation-thumbnails.js
@@ -16,10 +16,11 @@
 
                 var image = $(element.find('img'));
                 image.load( function() {
-                    var slides = image.height() / slideH - 1;
+                    var slides = image.height() / slideH;
                     var xDelta = slideW / slides;
                     image.mousemove(function (event){
-                        var curSlide = Math.floor((event.clientX)/xDelta);
+                        var curSlide = Math.floor((event.pageX - image.position().left) /xDelta);
+                        curSlide = ((curSlide < 0) ? 0 : curSlide);
                         curSlide = ((curSlide > slides) ? slides : curSlide);
                         image.css('top',-1 * slideH * curSlide + 'px');
                     });
